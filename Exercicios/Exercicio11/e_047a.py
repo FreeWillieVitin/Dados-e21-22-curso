8aeb34edbf87a25ccdff8a1821c00f197e2b9938
Victor <tirinho123@gmail.com>
2022-05-19 21:58:09 -0300

    Exercicios de funçoes e dicionarios

---
+++ Exercicios/Exercicio11/e_047a.py
@@ -0,0 +1,7 @@
+#e_047a.py
+
+def funcao1(num):
+    return num **3
+
+num = int(input('Digite um numero: '))
+retorno = funcao1(num)
---
+++ Exercicios/Exercicio11/e_047b.py
@@ -0,0 +1,15 @@
+#e_047b.py
+
+data = {
+    'Notas': [],
+    'Faltas': []
+}
+data['Notas'].append('10')
+data['Notas'].append('20')
+data['Notas'].append('30')
+data['Notas'].append('40')
+data['Faltas'].append('12')
+
+
+print(data)
+print(type(data))
