3c09f0237e709a63d8908b31e592fb58a0fd4de6
FreeWillieVitin <76778540+FreeWillieVitin@users.noreply.github.com>
2022-06-03 08:00:59 -0300

    Create pessoa_v3.py

---
+++ ExercicioPython/Exercicios_py_POO/pessoa_v3.py
@@ -0,0 +1,12 @@
+class Pessoa:
+  def __init__(self,nome,idade,sexo=false,altura=false)
+    self.nome = nome
+    self.idade = idade
+    self.sexo = sexo
+    self.altura = altura
+
+pessoa1 = Pessoa('Victor', 47, 'M', 1.74)
+pessoa2= Pessoa('Marieli', 23)
+
+print(pessoa1.nome,pessoa1.idade,pessoa1.sexo,pessoa1.altura)
+print(pessoa2.nome,pessoa2.idade,pessoa2.sexo,pessoa2.altura)
