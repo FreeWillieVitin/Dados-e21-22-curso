37cee805c7e7df2c6a2fda3840c2b388f88ba3c1
FreeWillieVitin <76778540+FreeWillieVitin@users.noreply.github.com>
2022-06-03 09:20:37 -0300

    Update e_069.py

--- ExercicioPython/e_069.py
+++ ExercicioPython/e_069.py
@@ -3,4 +3,11 @@
 #a serem cadastrados manualmente pelo usuario, simulando um simples carrinho de compras.
 
 class Inventario:
-    item1 =
+  def __init__(self,item1,item2):
+    self.item1 = item1
+    self.item2 = item2
+
+item1 = input('Digite um item: ')
+item2 = input('Digite um item: ')
+
+print(f'Produto {item1} e produto {item2} foram cadastrados')
