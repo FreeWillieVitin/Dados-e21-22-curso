05e52c9aa4f61199f7bf8a8b443a9ab12df5c896
FreeWillieVitin <76778540+FreeWillieVitin@users.noreply.github.com>
2022-06-07 08:46:44 -0300

    Update e_078.py

--- ExercicioPython/e_078.py
+++ ExercicioPython/e_078.py
@@ -1,7 +1,6 @@
 #e_078.py
 #Escreva um programa que recebe um texto do usuário e o converte para código morse, 
 #exibindo em tela o texto em formato Morse, segundo a padronização '.-' (Ponto, traço)
-# Python program to implement Morse Code Translator
 
 morse = ' '
 
