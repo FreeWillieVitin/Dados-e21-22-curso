3349c48f99c1defd59863405dd836307fa681212
FreeWillieVitin <76778540+FreeWillieVitin@users.noreply.github.com>
2022-06-07 10:13:08 -0300

    Update e_079.py
    Encontrei na net essa biblioteca que faz a conversão dos numeros

--- ExercicioPython/e_079.py
+++ ExercicioPython/e_079.py
@@ -1,2 +1,11 @@
 #e_079.py
 #Escreva um programa que recebe do usuário um número de 0 a 100 e transcreve o mesmo por extenso.
+from num2words import num2words
+
+numero = int( input('Digite um número: ') )
+
+num_ptbr = num2words(numero, lang='pt-br')
+print(f'Número: {num_ptbr}')
+
+num_ptbr_ord = num2words(numero, lang='pt-br', to='ordinal')
+print(f'Número (ordinal): {num_ptbr_ord}')
