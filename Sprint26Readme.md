715c1b878c9d475ed2e3cee3953e8ad100c86cfd
Victor Hugo Jurczyszyn <tirinho123@gmail.com>
2022-08-05 20:34:16 -0300

    exercicio banco de dados com pandas

---
+++ Sprint26Readme.md
@@ -0,0 +1,7 @@
+**Aula dia 04/08/2022**
+- Introdução a conexão sqlite com o python
+    - Como criar um novo Banco de Dados (Database com Python)
+    - Como se conectar ao Database
+    - Como Criar tabelas
+    - Como Inserir Dados
+    - Como selecionar os dados inseridos.
--- sqlite/modulo6/exercicio200/main.py
+++ sqlite/modulo6/exercicio200/main.py
@@ -1,3 +1,4 @@
+from random import randint
 import sqlite3
 import pandas as pd
 
@@ -6,13 +7,15 @@ cur = con.cursor()
 
 city = pd.read_csv('C:\\Users\\Victor\\Desktop\\idad.csv', delimiter=',')
 city.head()
+nome = pd.read_csv('C:\\Users\\Victor\\Desktop\\nomes.csv', delimiter=',')
+
 
 cur.execute("""     
 drop TABLE IF EXISTS cidades;
 """)
 
 cur.execute("""
-create table cidades (
+create table CIDADES (
     id_cidade INTEGER PRIMARY KEY AUTOINCREMENT,
     uf varchar(10) NOT NULL,
     nome_cidade varchar(50) NOT NULL
@@ -24,24 +27,48 @@ drop TABLE IF EXISTS pessoas;
 """)
 
 cur.execute("""
-CREATE TABLE pessoas (
+CREATE TABLE PESSOAS (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    CPF      varchar(14) NOT NULL,
     NOME     varchar(50) NOT NULL,
-    IDADE    INTEGER     NOT NULL,
-    ENDERECO    VARCHAR(50),
-    SALARIO     REAL,
+    IDADE    INTEGER ,
     ID_CIDADE VARCHAR(30)   
 );
 """)
 
+city.to_sql("CIDADES", con, if_exists='append', index=False)
 
-city.to_sql("cidades", con, if_exists='append', index=False)
+n_cidades = (len(city))
 
-cur.execute("Select * from cidades;")
-resultado = cur.fetchall()
-print(f'\n Os registros da tabela cidade é: {resultado}')
+nomes = pd.read_csv('C:\\Users\\Victor\\Desktop\\nomes.csv', sep=',')
+nome = nomes['NOME']
+for n in nome:
+    idade = randint(0, 100)
+    cidade = randint(1, n_cidades)
+    cur.execute("""
+        INSERT INTO PESSOAS
+            (NOME, IDADE, ID_CIDADE) 
+                VALUES
+            ('{}', '{}', '{}');""".format(n.upper(), idade, cidade))
 
-cur.execute("Select seq from sqlite_sequence;")
-total = cur.fetchall()
-print(f'\n A quantidade de registros na tabela é de: {total}')
+con.commit()
+
+
+# cur.execute("Select * from CIDADES;")
+# resultado = cur.fetchall()
+# print(f'\n Os registros da tabela cidade é: {resultado}\n')
+
+# cur.execute("SELECT COUNT(ID_CIDADE) FROM CIDADES")
+# total = cur.fetchall()
+# print(f'\n A quantidade de registros na tabela é de: {total}\n')
+
+# cur.execute("Select * from PESSOAS;")
+# resultado = cur.fetchall()
+# print(f'\n Os registros da tabela pessoas é: {resultado}\n')
+
+# cur.execute("SELECT COUNT(ID) FROM PESSOAS")
+# total = cur.fetchall()
+# print(f'\n A quantidade de registros na tabela é de: {total}\n')
+
+cur.execute("Select A.NOME, A.IDADE, B.NOME_CIDADE, B.UF FROM PESSOAS AS A, CIDADES AS B ORDER BY B.UF, B.NOME_CIDADE, A.NOME, A.IDADE;")
+resultado = cur.fetchmany(2)
+print(resultado)
